a646949e5fe42bb3ddee08d7efc6a8b5fe4c2998
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-02-19 19:37:19 +1100

    add use state to show icon in boxex and turn og game

--- src/component/Assets/TicTacToe.jsx
+++ src/component/Assets/TicTacToe.jsx
@@ -2,14 +2,93 @@ import React from "react";
 import "./TicTacToe.css";
 import circle_icon from "./circle.png";
 import cross_icon from "./cross.png";
+import { useState } from "react";
+let data = ["", "", "", "", "", "", "", "", ""];
 
 const TicTacToe = () => {
+  let [count, setCount] = useState(0);
+  let [lock, SetLock] = useState(false);
+  const toggle = (e, num) => {
+    if (lock) {
+      return 0;
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src="${cross_icon}">`;
+      data[num] = "x";
+      setCount(++count);
+    } else {
+      e.target.innerHTML = `<img src="${circle_icon}">`;
+      data[num] = "o";
+      setCount(++count);
+    }
+  };
   return (
     <div className="container">
       <h1 className="title">
         Tic Tac Toe Gmae In <span>React</span>
       </h1>
-      <div className="board"></div>
+      <div className="board">
+        <div className="row1">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 0);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 1);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 2);
+            }}
+          ></div>
+        </div>
+        <div className="row2">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 3);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 4);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 5);
+            }}
+          ></div>
+        </div>
+        <div className="row3">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 6);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 7);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 8);
+            }}
+          ></div>
+        </div>
+      </div>
       <button className="reset">Reset</button>
     </div>
   );
